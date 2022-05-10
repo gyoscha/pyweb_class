3f06f475ace4c8831b2efc61df2b8e5086c5279c
gyoscha <sokolovgeorgy@gmail.com>
2022-05-10 14:39:52 +0300

    add login (post)

--- login/views.py
+++ login/views.py
@@ -1,8 +1,11 @@
 from django.views import View
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
 
 
 class LoginView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'login/index.html')
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        return JsonResponse(request.POST, json_dumps_params={'indent': 4})
