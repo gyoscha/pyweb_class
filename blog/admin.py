2f3245eb1783f282d3b862daddfd270459739946
geosok <sokolovgeorgy@gmail.com>
2022-05-26 19:29:26 +0300

    add CommentInline to NoteAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,20 @@ from django.contrib import admin
 from . import models
 
 
+# https://docs.djangoproject.com/en/4.0/ref/contrib/admin/#inlinemodeladmin-objects
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+
+    extra = 0
+    min_num = 0
+
+
 @admin.register(models.Note)
 class NoteAdmin(admin.ModelAdmin):
-    pass
+    # отображение связи Many-to-one
+    inlines = [
+        CommentInline
+    ]
 
 
 @admin.register(models.Comment)
